6e0290eec64d1bc7962c4889261a2fc5a8168d00
m2nlight <wingingbob@gmail.com>
2025-06-20 19:58:17 +0800

    nvim: open folder

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -5,7 +5,20 @@
 local m = vim.keymap
 -- m.set("i", "jj", "<esc>")
 
-local function SmartCloseOrBd()
+local function open_folder()
+  if vim.fn.has("win32") == 1 then
+    local path = vim.fn.expand("%:p")
+    path = path:gsub("/", "\\")
+    vim.cmd('silent !explorer /select,"' .. path .. '"')
+  elseif vim.fn.has("macunix") == 1 then
+    local path = vim.fn.expand("%:p:h")
+    vim.cmd('silent !open "' .. path .. '"')
+  else
+    vim.notify("Unsupported OS", vim.log.levels.WARN)
+  end
+end
+
+local function smart_close()
     local win_count = vim.fn.tabpagewinnr(vim.fn.tabpagenr(), '$')
     if win_count > 1 then
         vim.cmd('close')
@@ -42,17 +55,17 @@ m.set("n", "<space>-", "<c-w>s", { desc = "Split Window" })
 m.set("n", "<space>/", function() vim.wo.wrap = not vim.wo.wrap end, { desc = "Toggle Wrap" })
 m.set("n", "<space>\\", function() Snacks.git.blame_line() end, { desc = "Git Blame" })
 
-m.set("n", "<space>q", function() SmartCloseOrBd() end, { desc = "Close Window or Buffer" })
+m.set("n", "<space>q", function() smart_close() end, { desc = "Smart Close" })
 m.set("n", "<space>Q", "<cmd>qa<cr>", { desc = "Quit All" })
 m.set("n", "<space>w", "<cmd>w!<cr>", { desc = "Write File" })
 m.set("n", "<space>W", "<cmd>w !sudo tee %<cr>", { desc = "Write File with Super User" })
 
 ---@diagnostic disable-next-line: missing-fields
-m.set("n", "<space>e", function() Snacks.explorer( { cwd = LazyVim.root() } ) end, { desc = "File Explorer" })
-m.set("n", "<space>E", function() Snacks.picker.projects() end, { desc = "Projects" })
+m.set("n", "<space>e", function() Snacks.explorer({ cwd = LazyVim.root() }) end, { desc = "File Explorer" })
+m.set("n", "<space>E", function() Snacks.explorer.reveal() end, { desc = "File Explorer (Reveal)" })
 m.set("n", "<space>r", function() return ":IncRename " .. vim.fn.expand("<cword>") end, { expr = true, desc = "IncRename" })
 m.set({"n", "v"}, "<space>R", function() require('grug-far').open({ prefills = { search = vim.fn.expand("<cword>") } }) end, { desc = "Search and Replace" })
-m.set("n", "<space>t", function() Snacks.picker.grep() end, { desc = "Grep" })
+m.set("n", "<space>t", function() Snacks.picker.grep({ cwd = LazyVim.root() }) end, { desc = "Grep" })
 
 ---@diagnostic disable-next-line: undefined-field
 m.set("n", "<space>T", function() Snacks.picker.todo_comments() end, { desc = "Todo" })
@@ -65,8 +78,9 @@ m.set({ "n", "v" }, "<space>Y", "<cmd>:SwitchReverse<cr>", { desc = "Switch Reve
 m.set("n", "<space>u", function() require("neotest").run.run({vim.fn.expand("%"), strategy = "dap"}) end, { desc = "Test Run (Neotest)" })
 m.set("n", "<space>U", function() require("neotest").summary.toggle() end, { desc = "Test Summary (Neotest)" })
 m.set("n", "<space>i", function() Snacks.picker.lsp_implementations() end, { desc = "Goto Implementation" })
-m.set("n", "<space>o", function() Snacks.explorer.reveal() end, { desc = "File Explorer (Reveal)" })
+m.set("n", "<space>o", function() open_folder() end, { desc = "Open Folder" })
 m.set("n", "<space>p", "<cmd>BufferLineTogglePin<cr>", { desc = "Toggle Pin" })
+m.set("n", "<space>P", function() Snacks.picker.projects() end, { desc = "Projects" })
 
 m.set({ "n", "v" }, "<space>a", function() vim.lsp.buf.code_action() end, { desc = "Code Action" })
 m.set("n", "<space>s", function() Snacks.picker.lsp_workspace_symbols() end, { desc = "LSP Workspace Symbols" })
